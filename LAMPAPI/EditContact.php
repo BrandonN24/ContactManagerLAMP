a7b7c805b37fde125444d818759803c693088e98
Brandon Nguyen <brandonlnguyen0812@gmail.com>
2023-01-26 13:39:00 -0500

    Added comments, updated SQL command to match edit functionality

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -1,42 +1,44 @@
 <?php
 	$inData = getRequestInfo();
-	
-   // Capturing input from user
-  $firstName = $inData["firstName"];
-  $lastName = $inData["lastName"];
-  $phone = $inData["phone"];
-  $email = $inData["email"];
-  $userId = $inData["userId"];
- 
+
+	// Capturing input from user
+	$firstName = $inData["firstName"]; // take in a new value for the firstName
+	$lastName = $inData["lastName"]; // take in a new value for the lastName
+	$phone = $inData["phone"];	// take in a new value for the phone number
+	$email = $inData["email"];	// take in a new value for the email
+	$ID = $inData["id"]; // take in the unique ID of the contact to be edited
+
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
- 
-  // Check for spaces in the inputs and remove them
-  $firstName = str_replace(" ", "", $firstName);
-  $lastName = str_replace(" ", "", $lastName);
-  $phone = str_replace(" ", "", $phone);
-  $email = str_replace(" ", "", $email);
- 
+
+	// Check for spaces in the inputs and remove them
+	// This is done to clean up input to be stored into the database
+	$firstName = str_replace(" ", "", $firstName);
+	$lastName = str_replace(" ", "", $lastName);
+	$phone = str_replace(" ", "", $phone);
+	$email = str_replace(" ", "", $email);
+
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	}
-  // Checking to see if any of the fields are blank or null.
-	else if($firstName == NULL || $lastName == NULL || $phone == NULL || $email == NULL || $userId == NULL){
-      returnWithError("One of the fields are null");
-  }
-  // now make the check for empty strings  (usually, the NULL checks will catch this)
-  else if($firstName == "" || $lastName == "" || $phone == "" || $email == "" || $userId == ""){
-      returnWithError("One of the fields are blank");
-  }
-  else
+	// Checking to see if any of the fields are blank or null.
+	else if($firstName == NULL || $lastName == NULL || $phone == NULL || $email == NULL){
+		returnWithError("One of the fields are null");
+	}
+	// now make the check for empty strings  (usually, the NULL checks will catch this)
+	else if($firstName == "" || $lastName == "" || $phone == "" || $email == ""){
+		returnWithError("One of the fields are blank");
+	}
+	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
+		// unique ID of contact must be known to update it.
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE ID = ?");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
-		returnWithError("Contact created successfully");
+		returnWithError("Contact updated successfully");
 	}
 
 	function getRequestInfo()
