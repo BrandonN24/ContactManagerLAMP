8b67abc071c7aa50ba5132cc637eb70dbbca7f1f
Brandon Nguyen <brandonlnguyen0812@gmail.com>
2023-01-27 17:23:18 -0500

    Added unique-username requirements to Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -5,6 +5,7 @@
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
+	$username = $inData["login"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
@@ -13,12 +14,27 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?, ?, ? ,?)");
-		$stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+		$stmt = $conn->prepare("select Login from Users WHERE Login = ?");
+		$stmt->bind_param("s", $username);
 		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+
+		$result = $stmt->get_result();
+
+		// if no results found, continue to register
+		if(!$result->fetch_assoc()){
+			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?, ?, ? ,?)");
+			$stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $username, $inData["password"]);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
+		// otherwise, throw an error
+		else{
+			returnWithError("Username taken.");
+		}
+
+
 	}
 	
 	function getRequestInfo()
